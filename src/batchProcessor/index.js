2438b2b7cb4592b84e6b8faa59bbf7aa806f31a4
Gabriel Foust <gfoust@harding.edu>
2026-03-20 16:50:20 -0500

    Project start

---
+++ src/batchProcessor/index.js
@@ -0,0 +1,8 @@
+
+export async function handler(event) {
+  // This is just a place-holder; replace it with your actual lambda function
+  return {
+    statusCode: 200,
+    body: { name: "batchProcessor" }
+  };
+};
